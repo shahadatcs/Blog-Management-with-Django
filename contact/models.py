89e8fd62b1f1e5058464fa888e755f39c71f7e58
shahadatcs <shahadat.pust@gmail.com>
2023-03-16 01:40:48 +0600

    update contact app

--- contact/models.py
+++ contact/models.py
@@ -8,6 +8,5 @@ class Contact(models.Model):
   subject = models.TextField(blank=True)
   message = models.TextField(blank=True)
   contact_date = models.DateTimeField(default=datetime.now, blank=True)
-  user_id = models.IntegerField(blank=True)
   def __str__(self):
     return self.name
--- contact/views.py
+++ contact/views.py
@@ -2,34 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from .models import Contact
+from . forms import ContactForm
+
 
 def contact(request):
-  if request.method == 'POST':
-    name = request.POST['name']
-    email = request.POST['email']
-    subject = request.POST['subject']
-    message = request.POST['message']
-    user_id = request.POST['user_id']
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been submitted, Admin will get back to you soon')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
 
-    #  Check if user has made message already
-    if request.user.is_authenticated:
-      user_id = request.user.id
-      has_contacted = Contact.objects.all().filter(user_id=user_id)
-      if has_contacted:
-        messages.error(request, 'You have already send message')
-    contact = Contact(name=name, email=email, subject=subject, message=message)
-    contact.save()
 
-    # Send email
-    # send_mail(
-    #   'Blog message send',
-    #   'There has been an message for ',
-    #   'admin@gmail.com',
-    #   [user_email, 'shahadat@gmail.com'],
-    #   fail_silently=False
-    # )
-
-    messages.success(request, 'Your message has been submitted, Admin will get back to you soon')
-  else:
-     return render(request, 'contact/contact.html')
 
